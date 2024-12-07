9b17db36949edf7bb62e088d57ead280ee78e921
akbar904 <zharfan@walturn.com>
2024-12-07 17:18:31 +0700

    Create text_themes.dart

---
+++ src/lib/ui/common/text_themes.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+// Display Styles
+const TextStyle displayLargeStyle = TextStyle(
+  fontSize: 96,
+  fontWeight: FontWeight.w300,
+  letterSpacing: -1.5,
+);
+
+const TextStyle displayMediumStyle = TextStyle(
+  fontSize: 60,
+  fontWeight: FontWeight.w300,
+  letterSpacing: -0.5,
+);
+
+const TextStyle displaySmallStyle = TextStyle(
+  fontSize: 48,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+);
+
+// Headline Styles
+const TextStyle headlineLargeStyle = TextStyle(
+  fontSize: 40,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.25,
+);
+
+const TextStyle headlineMediumStyle = TextStyle(
+  fontSize: 34,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.25,
+);
+
+const TextStyle headlineSmallStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+);
+
+// Title Styles
+const TextStyle titleLargeStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.15,
+);
+
+const TextStyle titleMediumStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.15,
+);
+
+const TextStyle titleSmallStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.1,
+);
+
+// Body Styles
+const TextStyle bodyLargeStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.5,
+);
+
+const TextStyle bodyMediumStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.25,
+);
+
+const TextStyle bodySmallStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.4,
+);
+
+// Label Styles
+const TextStyle labelLargeStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 1.25,
+);
+
+const TextStyle labelMediumStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 1.5,
+);
+
+const TextStyle labelSmallStyle = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 1.5,
+);
